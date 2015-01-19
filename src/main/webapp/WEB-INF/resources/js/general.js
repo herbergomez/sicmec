c75222f4b63c56e990c7d7bac895530acc5800f5
pabloxtiyo <oswalxtiyo@gmail.com>
2015-01-19 15:58:52 -0600

    Mejoras al login y arreglo de bugs

--- src/main/webapp/WEB-INF/resources/js/general.js
+++ src/main/webapp/WEB-INF/resources/js/general.js
@@ -3,6 +3,47 @@
  */
 $(document).ready(function() 
 {
+	/*
+	 * Formato general 
+	 * para fechas que requieren
+	 * horas
+	 * */
+	$('.datetimepicker').datetimepicker({
+		dateFormat: "yy-mm-dd",
+	    timeFormat: "HH:mm:ss",
+	    	currentText: "Ahora",
+	    	closeText: "Listo",
+	    	timeText: 'Hora',
+            hourText: 'Horas',
+            minuteText: 'Minutos',
+            secondText: 'Segundos',
+            millisecText: 'Milisegundos',
+	    	dayNamesMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa"],
+	    	monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo","Junio", "Julio", "Agosto", "Septiembre","Octubre", "Noviembre", "Diciembre"],
+	    	monthNamesShort: ["Ene", "Feb", "Mar", "Abr","May", "Jun", "Jul", "Ago","Sep", "Oct", "Nov", "Dic"]
+	});	
+	/*
+	 * Formato general 
+	 * fechas
+	 * */
+	$('.datepicker').datepicker({
+		closeText: 'Cerrar',
+        prevText: 'Ant',
+        nextText: 'Sig',
+        currentText: 'Hoy',
+        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+        dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
+        dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sa'],
+        weekHeader: 'Sm',
+        dateFormat: 'yy-mm-dd',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+	});	
+	
 	$("#formCambioClave").validate({
 	errorElement: "span",
 	rules: 
